dee6a35279847919dcbf2e2b25d4c542aeceee66
matheusmrs01 <matheus.silva.morais01@gmail.com>
2022-05-06 17:12:55 -0300

    fix: usando o dto dos parametros do ticket

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TicketService } from './ticket.service';
+import { TicketParamsDto } from './dtos/params.dto';
 
 @Controller('boleto')
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
-  @Get('')
-  async findTicket() {
-    return this.ticketService.findTicket();
+  @Get(':ticketId')
+  async findTicket(@Param() params: TicketParamsDto) {
+    return this.ticketService.findTicket(params.ticketId);
   }
 }
