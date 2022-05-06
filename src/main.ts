266bb5831aca52b6f4f8164c089a0a5970814166
matheusmrs01 <matheus.silva.morais01@gmail.com>
2022-05-06 17:10:01 -0300

    fix: adicionando pipe de validação na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupGlobalPipes(app);
+
   await app.listen(8080);
 }
+
+function setupGlobalPipes(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validateCustomDecorators: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+}
+
 bootstrap();
