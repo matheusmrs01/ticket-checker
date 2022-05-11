d71be8bad705e0d90bce7f2e45586fd95aa4a8aa
matheusmrs01 <matheus.silva.morais01@gmail.com>
2022-05-11 17:29:55 -0300

    fix: adicionando mocks para linha digitavel com 48 caracteres

--- src/ticket/ticket.mocks.ts
+++ src/ticket/ticket.mocks.ts
@@ -2,10 +2,19 @@ import { TicketResponseDto } from './dtos/ticketResponse.dto';
 
 const correctDigitableLine = '21290001192110001210904475617405975870000002010';
 
+const correctDigitableLineWith48Characters =
+  '212900011921100012109044756174059758700000020000';
+
 const correctResponseValue = new TicketResponseDto({
   barCode: '21299758700000020100001121100012100447561740',
   amount: '20,10',
   expirationDate: '2018-07-16',
+});
+
+const correctResponseValueForOtherTest = new TicketResponseDto({
+  barCode: '21290001192100012109047561740597570000002000',
+  amount: '11.921,00',
+  expirationDate: null,
 });
 
 const incorrectDigitableLine =
@@ -19,4 +28,6 @@ export {
   correctResponseValue,
   incorrectDigitableLine,
   incorrectDigitableLineWithWords,
+  correctDigitableLineWith48Characters,
+  correctResponseValueForOtherTest,
 };
